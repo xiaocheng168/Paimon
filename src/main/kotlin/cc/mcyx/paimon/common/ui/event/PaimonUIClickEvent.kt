b43bb53fa8c14a033a27eac42418cb6b3aa873d6
Zcc <x3513312668@gmail.com>
2023-09-22 05:44:13 +0800

    GUI点击事件以及物品按钮点击事件信息类

---
+++ src/main/kotlin/cc/mcyx/paimon/common/ui/event/PaimonUIClickEvent.kt
@@ -0,0 +1,13 @@
+package cc.mcyx.paimon.common.ui.event
+
+import cc.mcyx.paimon.common.ui.PaimonUI
+import org.bukkit.inventory.ItemStack
+
+/**
+ * 是否取消事件
+ * @param paimonUI 界面对象
+ * @param itemStack 按钮物品
+ * @param slot 按钮位置
+ * @param isCancel 是否取消
+ */
+class PaimonUIClickEvent(val paimonUI: PaimonUI, val itemStack: ItemStack, val slot: Int, var isCancel: Boolean)
