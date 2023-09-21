93d36cbb151e417900d064b0ad4fa02c6430cdac
Zcc <x3513312668@gmail.com>
2023-09-22 05:43:45 +0800

    新GUI界面，它使用了NMS发包，避免如果出错玩家刷物品的问题哦~

--- src/main/kotlin/cc/mcyx/paimon/common/ui/PaimonUI.kt
+++ src/main/kotlin/cc/mcyx/paimon/common/ui/PaimonUI.kt
@@ -1,106 +1,160 @@
 package cc.mcyx.paimon.common.ui
 
-import cc.mcyx.paimon.common.listener.PaimonAutoListener
-import cc.mcyx.paimon.common.minecraft.craftbukkit.registerListener
-import org.bukkit.Bukkit
-import org.bukkit.entity.Player
-import org.bukkit.event.EventHandler
-import org.bukkit.event.inventory.InventoryClickEvent
-import org.bukkit.event.inventory.InventoryCloseEvent
-import org.bukkit.event.inventory.InventoryOpenEvent
-import org.bukkit.event.inventory.InventoryType
-import org.bukkit.inventory.Inventory
+import cc.mcyx.paimon.common.minecraft.craftbukkit.CraftBukkitPacket
+import cc.mcyx.paimon.common.minecraft.network.PaimonPlayer
+import cc.mcyx.paimon.common.ui.event.PaimonUIClickEvent
 import org.bukkit.inventory.ItemStack
+import kotlin.math.min
 
-open class PaimonUI(private var inventoryType: InventoryType, private var title: String) : PaimonAutoListener {
-    private var inventory: Inventory = Bukkit.createInventory(null, this.inventoryType, this.title)
+class PaimonUI(paimonUIType: PaimonUIType, head: String = "空空如也") {
+    var paimonPlayer: PaimonPlayer? = null
+    private var gid = 0
+    private val buttons: HashMap<ItemStack, ButtonInfo> = hashMapOf()
 
-    @EventHandler
-    fun click(e: InventoryClickEvent) {
-        if (e.view.title == this.inventory.title) {
-            click?.invoke(e)
-            val clickButton = e.currentItem
-            //点击的物品不能是空
-            if (clickButton != null && e.whoClicked is Player) {
-                //从监听回调里寻找，回调函数 且回调此事件
-                buttons[clickButton]?.invoke(e.whoClicked as Player, e.slot, clickButton)
+    var head: String
+    var paimonUIType: PaimonUIType
+
+    var isOpen: Boolean = false
+
+    init {
+        this.head = head
+        this.paimonUIType = paimonUIType
+    }
+
+    fun open(paimonPlayer: PaimonPlayer): PaimonUI {
+
+        this.paimonPlayer = paimonPlayer
+        this.gid = paimonPlayer.nextContainerCounter()
+
+        //打开界面
+        paimonPlayer.sendPacket(
+            CraftBukkitPacket.createGUIPacket(
+                this.gid,
+                this.paimonUIType.type,
+                this.head,
+                this.paimonUIType.size
+            )
+        )
+        this.isOpen = true
+        //批量发送物品位置包
+        for (value in buttons.values) {
+            sendSetItemPacket(value.slot, value.itemStack)
+        }
+
+        paimonPlayer.packetListener { it ->
+            //玩家点击某个地方与按钮
+            if (it.packet::class.java == CraftBukkitPacket.asNMSClass("PacketPlayInWindowClick")) {
+                val packetPlayInWindowClick = CraftBukkitPacket.asNMSClass("PacketPlayInWindowClick").cast(it.packet)
+                packetPlayInWindowClick.javaClass.getDeclaredField("item").also {
+                    it.isAccessible = true
+                    //解析读取Item
+                    CraftBukkitPacket.nmsItemToItemStack(
+                        CraftBukkitPacket.itemStack.cast(
+                            it.get(
+                                packetPlayInWindowClick
+                            )
+                        )
+                    ).apply {
+                        val paimonUIClickEvent = PaimonUIClickEvent(
+                            this@PaimonUI,
+                            this,
+                            it.javaClass.getDeclaredField("slot").apply { isAccessible = true }.get(it) as Int,
+                            false
+                        )
+                        clickEvent?.invoke(paimonUIClickEvent)!!
+                        //如果取消了将刷新界面
+                        if (paimonUIClickEvent.isCancel) {
+                            this@PaimonUI.update()
+                        }
+                        //触发按钮独立事件
+                        buttons[this]?.callback?.invoke(paimonUIClickEvent)
+                        //如果取消了将刷新界面
+                        if (paimonUIClickEvent.isCancel) {
+                            this@PaimonUI.update()
+                        }
+                    }
+                }
+            }
+            //如果玩家关闭界面
+            if (it.packet::class.java == CraftBukkitPacket.asNMSClass("PacketPlayInCloseWindow")) {
+                if (CraftBukkitPacket.asNMSClass("PacketPlayInCloseWindow").getDeclaredField("id").apply {
+                        isAccessible = true
+                    }.get(it.packet) == this.gid) {
+                    this.isOpen = false
+                    //回调事件用于处理
+                    this.closeEvent?.invoke(this)
+                }
             }
         }
-    }
-
-    @EventHandler
-    fun closeGUI(e: InventoryCloseEvent) {
-        if (e.view.title == this.inventory.title) {
-            close?.invoke(e)
-        }
-    }
-
-    @EventHandler
-    fun closeGUI(e: InventoryOpenEvent) {
-        if (e.view.title == this.inventory.title) {
-            open?.invoke(e)
-        }
-    }
-
-    private var click: ((InventoryClickEvent) -> Unit)? = null
-    private var open: ((InventoryOpenEvent) -> Unit)? = null
-    private var close: ((InventoryCloseEvent) -> Unit)? = null
-    private var buttons: HashMap<ItemStack, ((player: Player, slot: Int, item: ItemStack) -> Unit)> = hashMapOf()
-
-    /**
-     * 界面点击事件
-     * @param event 回调事件
-     */
-    fun click(event: ((InventoryClickEvent) -> Unit)): PaimonUI {
-        this.click = event
         return this
     }
 
     /**
-     * 界面点击事件
-     * @param event 回调事件
+     * 更新交互界面
+     * 界面必须处于打开界面才可继续更新交互界面
      */
-    fun open(event: ((InventoryOpenEvent) -> Unit)): PaimonUI {
-        this.open = event
+    fun update() {
+        if (isOpen)
+            if (this.paimonPlayer != null) {
+                //更新玩家背包
+                paimonPlayer?.player?.updateInventory()
+                //更新玩家界面
+                this.open(this.paimonPlayer!!)
+            } else throw RuntimeException("the gui not init.. please invoke PaimonUI.open()")
+    }
+
+    //关闭界面事件
+    private var closeEvent: ((PaimonUI) -> Unit)? = null
+
+    //点击事件 点击事件为界面所有地方点击反馈 它的优先级大于 > itemClickEvent
+    private var clickEvent: ((PaimonUIClickEvent) -> Unit)? = null
+
+    //按钮点击回调
+    private var itemClickEvent: ((PaimonUIClickEvent) -> Unit)? = null
+
+    fun closeEvent(e: (PaimonUI) -> Unit) {
+        this.closeEvent = e
+    }
+
+    fun clickEvent(e: (PaimonUIClickEvent) -> Unit) {
+        this.clickEvent = e
+    }
+
+    fun setItem(slot: Int, itemStack: ItemStack, itemClick: ((PaimonUIClickEvent) -> Unit)? = null): PaimonUI {
+        //设置回调函数
+        this.itemClickEvent = itemClick
+        //设置该物品的点击事件
+        buttons[itemStack] = ButtonInfo(itemStack, slot, itemClick)
+        //发送设置物品包
+        sendSetItemPacket(slot, itemStack)
         return this
     }
 
     /**
-     * 界面点击事件
-     * @param event 回调事件
-     */
-    fun close(event: ((InventoryCloseEvent) -> Unit)): PaimonUI {
-        this.close = event
-        return this
-    }
-
-    /**
-     * 设置按钮图标
+     * 发送物品按钮包
+     * @param itemStack 物品堆
      * @param slot 位置
-     * @param itemStack 物品
-     * @return 返回该界面对象
      */
-    fun setButton(
-        slot: Int,
-        itemStack: ItemStack,
-        buttonClick: ((player: Player, slot: Int, item: ItemStack) -> Unit)? = null
-    ): PaimonUI {
-        inventory.setItem(slot, itemStack)
-        //将此会函数加入HasMap调用时查找使用
-        if (buttonClick != null) {
-            buttons[itemStack] = buttonClick
-        }
-        return this
+    private fun sendSetItemPacket(slot: Int, itemStack: ItemStack) {
+        paimonPlayer?.sendPacket(
+            CraftBukkitPacket.createSlotItemPacket(
+                gid,
+                min(slot, this.paimonUIType.size - 1),
+                itemStack
+            )
+        )
     }
 
-    /**
-     * 给一个玩家打开该界面
-     * @param player 玩家
-     * @return 返回该界面对象
-     */
-    fun open(player: Player): PaimonUI {
-        player.openInventory(this.inventory)
-        registerListener(this)
-        return this
+
+    enum class PaimonUIType(val type: String, val size: Int) {
+        CHEST_9("minecraft:chest", 9),
+        CHEST_18("minecraft:chest", 18),
+        CHEST_24("minecraft:chest", 24),
+        CHEST_36("minecraft:chest", 36),
+        CHEST_48("minecraft:chest", 48),
+        CHEST_56("minecraft:chest", 56),
+        HOPPER("minecraft:hopper", 5),
+        ANVIL("minecraft:anvil", 3),
     }
+
 }
