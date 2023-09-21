3eacbe874d0d460f282b786beeae377349c943c6
Zcc <x3513312668@gmail.com>
2023-09-22 05:45:02 +0800

    代理玩家管理类，里面记录着所有在服务器中的玩家

---
+++ src/main/kotlin/cc/mcyx/paimon/common/minecraft/network/ProxyPlayerManager.kt
@@ -0,0 +1,34 @@
+package cc.mcyx.paimon.common.minecraft.network
+
+
+import org.bukkit.entity.Player
+
+abstract class ProxyPlayerManager {
+
+    companion object {
+        //代理玩家表
+        private val paimonPlayers: HashMap<Player, PaimonPlayer> = hashMapOf()
+
+        /**
+         * 添加代理玩家
+         * @param player BukkitPlayer 玩家对象
+         * @return 返回代理玩家
+         */
+        fun addPaimonPlayer(player: Player): PaimonPlayer {
+            return PaimonPlayer(player).also {
+                paimonPlayers[player] = it
+            }
+        }
+
+        /**
+         * 获取代理玩家
+         * @param player BukkitPlayer 玩家对象
+         * @return 代理玩家
+         */
+        fun getPaimonPlayer(player: Player): PaimonPlayer {
+            return if (!paimonPlayers.contains(player)) {
+                addPaimonPlayer(player)
+            } else paimonPlayers[player]!!
+        }
+    }
+}
