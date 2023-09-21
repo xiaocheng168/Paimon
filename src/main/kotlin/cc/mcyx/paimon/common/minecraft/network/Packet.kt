3557187190159f70c3496d6f98f3d326b21f9914
Zcc <x3513312668@gmail.com>
2023-09-22 05:40:52 +0800

    NMS 数据包数据类

---
+++ src/main/kotlin/cc/mcyx/paimon/common/minecraft/network/Packet.kt
@@ -0,0 +1,10 @@
+package cc.mcyx.paimon.common.minecraft.network
+
+/**
+ * 数据包信息类
+ * @param packet Packet数据包
+ * @param isCancel 是否拦截数据吧
+ */
+class Packet(
+    val packet: Any, var isCancel: Boolean
+)
