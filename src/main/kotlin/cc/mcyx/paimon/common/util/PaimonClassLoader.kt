aee127ccb3e517b4eaa68efdc198170824919de8
Zcc <x3513312668@gmail.com>
2023-09-23 06:34:24 +0800

    使用了Bukkit的加载器来扫

--- src/main/kotlin/cc/mcyx/paimon/common/util/PaimonClassLoader.kt
+++ src/main/kotlin/cc/mcyx/paimon/common/util/PaimonClassLoader.kt
@@ -1,11 +1,10 @@
 package cc.mcyx.paimon.common.util
 
-import cc.mcyx.paimon.common.plugin.Paimon
+import cc.mcyx.paimon.common.PaimonPlugin
 import cc.mcyx.paimon.common.listener.PaimonAutoListener
 import cc.mcyx.paimon.common.minecraft.craftbukkit.registerListener
-import org.bukkit.Bukkit
-import org.bukkit.event.Listener
-import java.util.*
+import cc.mcyx.paimon.common.plugin.Paimon
+import java.lang.reflect.Field
 import java.util.jar.JarFile
 
 open class PaimonClassLoader(val paimon: Paimon) {
@@ -13,25 +12,28 @@ open class PaimonClassLoader(val paimon: Paimon) {
      * 加载某个插件
      */
     open fun loadPlugin() {
-        val jarFile =
-            JarFile(paimon.cl.getResource("plugin.yml")!!.file.replace("file:/", "").replace("!/plugin.yml", ""))
+        val jarFile = JarFile(PaimonPlugin.getPluginJarFile().file)
         //加载类
         this.loadJarClass(jarFile).forEach { loadClass(it) }
+        //无法自动监听所有类
         //反射获取插件所有加载列表
-        val declaredField = ClassLoader::class.java.getDeclaredField("classes")
+        val declaredField: Field = Paimon::class.java.classLoader::class.java.getDeclaredField("classes")
         declaredField.isAccessible = true
-        val classes = declaredField.get(paimon.cl) as Vector<Class<*>>
+        val classes = declaredField.get(paimon.cl) as Map<String, Class<*>>
         //获取当前插件主类包路径
         val pluginPacket = Class.forName(paimon.description.main).`package`.name
         //获取插件加载类表
         val paimonPluginClass = classes.filter { it.toString().contains(pluginPacket) }
         //遍历加载列表
-        paimonPluginClass.iterator().forEach {
+        paimonPluginClass.values.iterator().forEach {
             //如果这个类不是接口
             if (!it.isInterface) {
                 //判断这个类是否存在 PaimonListener
                 if (it.interfaces.contains(PaimonAutoListener::class.java)) {
-                    registerListener(it.newInstance() as PaimonAutoListener)
+                    //如果是Paimon插件
+                    if (it.genericSuperclass == PaimonPlugin::class.java) {
+                        registerListener(paimon as PaimonAutoListener)
+                    } else registerListener(it.newInstance() as PaimonAutoListener)
                 }
             }
         }
