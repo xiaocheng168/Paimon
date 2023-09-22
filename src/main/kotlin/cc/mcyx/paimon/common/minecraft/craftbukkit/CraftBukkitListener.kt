4a087441bce22d486e54238cd94106b74b3de53a
Zcc <x3513312668@gmail.com>
2023-09-23 06:31:37 +0800

    添加了自动注册的监听类表

--- src/main/kotlin/cc/mcyx/paimon/common/minecraft/craftbukkit/CraftBukkitListener.kt
+++ src/main/kotlin/cc/mcyx/paimon/common/minecraft/craftbukkit/CraftBukkitListener.kt
@@ -4,10 +4,16 @@ import cc.mcyx.paimon.common.PaimonPlugin
 import cc.mcyx.paimon.common.listener.PaimonAutoListener
 import org.bukkit.Bukkit
 
+//自动注册监听表
+val autoRegisterListenerList: MutableList<PaimonAutoListener> = mutableListOf()
+
 /**
  * 注册监听器
  * @param paimonAutoListener 监听器接口
  */
 fun registerListener(paimonAutoListener: PaimonAutoListener) {
-    Bukkit.getPluginManager().registerEvents(paimonAutoListener, PaimonPlugin.paimonPlugin)
+    paimonAutoListener.also {
+        Bukkit.getPluginManager().registerEvents(it, PaimonPlugin.paimonPlugin)
+        autoRegisterListenerList.add(it)
+    }
 }
