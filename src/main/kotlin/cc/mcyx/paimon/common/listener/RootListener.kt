de14b282e0581417de736676442d23bb73b23d0a
Zcc <x3513312668@gmail.com>
2023-09-25 02:51:19 +0800

    ignoreCancelled 取消加入事件将不处理

--- src/main/kotlin/cc/mcyx/paimon/common/listener/RootListener.kt
+++ src/main/kotlin/cc/mcyx/paimon/common/listener/RootListener.kt
@@ -2,10 +2,14 @@ package cc.mcyx.paimon.common.listener
 
 import cc.mcyx.paimon.common.minecraft.network.ProxyPlayerManager
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.player.PlayerJoinEvent
 
 class RootListener : PaimonAutoListener {
-    @EventHandler
+    @EventHandler(
+        priority = EventPriority.HIGH,
+        ignoreCancelled = true
+    )
     fun joinEvent(e: PlayerJoinEvent) {
         ProxyPlayerManager.addPaimonPlayer(e.player)
     }
