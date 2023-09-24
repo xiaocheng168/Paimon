d1fcfd8d9832f7ef86a6817c886aa74a34570bc6
Zcc <x3513312668@gmail.com>
2023-09-24 17:27:22 +0800

    新增代理玩家

--- src/main/kotlin/cc/mcyx/paimon/common/ui/event/PaimonUIClickEvent.kt
+++ src/main/kotlin/cc/mcyx/paimon/common/ui/event/PaimonUIClickEvent.kt
@@ -1,5 +1,6 @@
 package cc.mcyx.paimon.common.ui.event
 
+import cc.mcyx.paimon.common.minecraft.network.PaimonPlayer
 import cc.mcyx.paimon.common.ui.PaimonUI
 import org.bukkit.inventory.ItemStack
 
@@ -10,4 +11,10 @@ import org.bukkit.inventory.ItemStack
  * @param slot 按钮位置
  * @param isCancel 是否取消
  */
-class PaimonUIClickEvent(val paimonUI: PaimonUI, val itemStack: ItemStack, val slot: Int, var isCancel: Boolean)
+class PaimonUIClickEvent(
+    val paimonUI: PaimonUI,
+    val paimonPlayer: PaimonPlayer,
+    val itemStack: ItemStack,
+    val slot: Int,
+    var isCancel: Boolean
+)
