9f3226b673fa6d82aa09fe4c297016e62df18b34
Zcc <3513312668@gmail.com>
2024-01-11 22:54:41 +0800

    调整了父子命令节点

--- src/main/kotlin/cc/mcyx/paimon/common/command/PaimonSubCommand.kt
+++ src/main/kotlin/cc/mcyx/paimon/common/command/PaimonSubCommand.kt
@@ -6,6 +6,10 @@ import cc.mcyx.paimon.common.plugin.Paimon
  * 子命令批处理类
  * @param paimon 插件主体
  * @param command 命令头
+ * @param parentCommand 父命令
  */
-class PaimonSubCommand(paimon: Paimon, command: String, permissionNode: String = "") :
-    PaimonCommand(paimon, command, permissionNode)
+class PaimonSubCommand(
+    paimon: Paimon,
+    command: String,
+    parentCommand: PaimonCommand? = null,
+) : PaimonCommand(paimon, command, parentCommand)
