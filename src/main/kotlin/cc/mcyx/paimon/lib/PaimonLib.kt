9fa2e7ef892dd3bca070db2b6cbde5df2674fe3e
Zcc <3513312668@gmail.com>
2024-01-21 01:52:09 +0800

    1065

--- src/main/kotlin/cc/mcyx/paimon/lib/PaimonLib.kt
+++ src/main/kotlin/cc/mcyx/paimon/lib/PaimonLib.kt
@@ -1,8 +1,6 @@
 package cc.mcyx.paimon.lib
 
 import cc.mcyx.paimon.common.PaimonPlugin
-import cc.mcyx.paimon.common.command.PaimonCommand
-import cc.mcyx.paimon.common.command.PaimonSubCommand
 import org.bukkit.Bukkit
 
 class PaimonLib : PaimonPlugin() {
@@ -10,25 +8,5 @@ class PaimonLib : PaimonPlugin() {
         logger.info("PaimonLib Enable")
         logger.info("Server ${Bukkit.getVersion()}")
         logger.info("Version ${getPluginYmlConfig().getString("version")}")
-
-        val paimonCommand = PaimonCommand(this, "nihao")
-
-        paimonCommand.addSubCommand(PaimonSubCommand(this, "halo1", paimonCommand)
-            .also { it ->
-                it.addSubCommand(PaimonSubCommand(this, "nia", it))
-                it.addSubCommand(PaimonSubCommand(this, "open", it))
-                it.addSubCommand(PaimonSubCommand(this, "aaa", it).also {
-                    it.addSubCommand(
-                        PaimonSubCommand(
-                            this,
-                            "wc66",
-                            it
-                        )
-                    )
-                })
-                it.addSubCommand(PaimonSubCommand(this, "ccc", it))
-            })
-        paimonCommand.addSubCommand(PaimonSubCommand(this, "b", paimonCommand))
-        paimonCommand.register()
     }
 }
