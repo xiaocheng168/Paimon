2733c53893e64fcb77c4a0efac812d1da544e818
Zcc <3513312668@gmail.com>
2024-02-19 00:49:21 +0800

    新增书界面支持1.14+

--- src/main/kotlin/cc/mcyx/paimon/common/ui/BookUI.kt
+++ src/main/kotlin/cc/mcyx/paimon/common/ui/BookUI.kt
@@ -2,11 +2,27 @@ package cc.mcyx.paimon.common.ui
 
 import cc.mcyx.paimon.common.minecraft.craftbukkit.CraftBukkitPacket
 import cc.mcyx.paimon.common.minecraft.network.PaimonPlayer
+import de.tr7zw.nbtapi.NBTItem
+import de.tr7zw.nbtapi.NBTList
+import de.tr7zw.nbtapi.handler.NBTHandlers
+import de.tr7zw.nbtapi.iface.NBTHandler
+import net.md_5.bungee.api.chat.BaseComponent
+import net.md_5.bungee.api.chat.HoverEvent
+import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.BookMeta
 
-class BookUI(val player: Player) {
+/**
+ * 虚拟书本UI
+ * @param player 玩家
+ * @param pagesLine 书本里的每一行数据
+ */
+class BookUI(
+    val player: Player,
+    private vararg val pagesLine: BaseComponent = arrayOf()
+) {
     companion object {
         @JvmStatic
         val PacketPlayOutOpenBook = CraftBukkitPacket.asNMSPacketClass("PacketPlayOutOpenBook")
@@ -18,14 +34,25 @@ class BookUI(val player: Player) {
     private val paimonPlayer = PaimonPlayer(player)
 
     fun open() {
+        val itemStack = ItemStack(Material.WRITTEN_BOOK).also {
+            val itemMeta = it.itemMeta as BookMeta
+            itemMeta.author = ""
+            itemMeta.title = ""
+            itemMeta.spigot().setPages(pagesLine)
+            it.itemMeta = itemMeta
+        }
+        //发送书物品
+        setInventoryItem(player.inventory.heldItemSlot, itemStack)
+        //打开书
         if (CraftBukkitPacket.serverId >= 1140) {
             val action =
                 EnumHand.getDeclaredField(if (CraftBukkitPacket.serverId >= 1170) "a" else "MAIN_HAND").get(null)
             paimonPlayer.sendPacket(
                 PacketPlayOutOpenBook.getConstructor(EnumHand).newInstance(action)
             )
-            setInventoryItem(player.inventory.heldItemSlot, ItemStack(Material.APPLE))
         }
+
+        player.updateInventory()
     }
 
     private fun setInventoryItem(slot: Int, itemStack: ItemStack) {
