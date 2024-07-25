95734a3aaad5f0cec1c35b5e52f99b97bb54aa3b
Zcc <x3513312668@gmail.com>
2024-07-25 17:21:46 +0800

    调整了小问题

--- src/main/kotlin/cc/mcyx/paimon/support/plugin/VelocityBoot.java
+++ src/main/kotlin/cc/mcyx/paimon/support/plugin/VelocityBoot.java
@@ -1,22 +1,22 @@
 package cc.mcyx.paimon.support.plugin;
 
 
-import cc.mcyx.paimon.support.IPaimonBoot;
-import com.velocitypowered.api.plugin.PluginContainer;
+//import cc.mcyx.paimon.support.IPaimonBoot;
+//import com.velocitypowered.api.plugin.PluginContainer;
 
-public class VelocityBoot implements IPaimonBoot<PluginContainer> {
-    @Override
-    public void init(PluginContainer plugin) {
-
-    }
-
-    @Override
-    public void enable(PluginContainer plugin) {
-
-    }
-
-    @Override
-    public void disable(PluginContainer plugin) {
-
-    }
-}
+//public class VelocityBoot implements IPaimonBoot<PluginContainer> {
+//    @Override
+//    public void init(PluginContainer plugin) {
+//
+//    }
+//
+//    @Override
+//    public void enable(PluginContainer plugin) {
+//
+//    }
+//
+//    @Override
+//    public void disable(PluginContainer plugin) {
+//
+//    }
+//}
